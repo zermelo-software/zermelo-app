259343b3f3c73b046033a4a0b2b89e2dfedd5272
Ulbe van der Werf <uvanderwerf@zermelo.nl>
2016-08-04 11:45:11 +0200

    Remove cross-platform check that only causes issues

--- www/app/controller/MainController.js
+++ www/app/controller/MainController.js
@@ -47,12 +47,7 @@ Ext.override(Ext.util.SizeMonitor, {
 });
 Ext.override(Ext.util.PaintMonitor, {
    constructor: function(config) {
-       if (Ext.browser.is.Firefox || (Ext.browser.is.WebKit && Ext.browser.engineVersion.gtEq('536') && !Ext.browser.engineVersion.ltEq('537.36') && !Ext.os.is.Blackberry)) {
-           return new Ext.util.paintmonitor.OverflowChange(config);
-       }
-       else {
-           return new Ext.util.paintmonitor.CssAnimation(config);
-       }
+        return new Ext.util.paintmonitor.CssAnimation(config);
    }
 });
 
